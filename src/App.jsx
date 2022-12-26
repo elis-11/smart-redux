74be7bd701f21e0e27e7e8fce78facfda3f6dc34
elis-11 <eliza.arzanukaeva@gmail.com>
2022-12-26 19:52:37 +0100

    First-Redux - CountSlice

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,49 @@ import { Cart } from "./pages/Cart";
 import { NotFound } from "./components/notfound/NotFound.jsx";
 import { Header } from "./pages/Header";
 import { createContext, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, test } from "./redux/slices/filterSlice";
 
 export const DataContext = createContext();
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="collumn items-center bg-orange-50">
-      <DataContext.Provider value={{searchValue, setSearchValue }}>
+
+      {/* <DataContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <Routes>
           <Route path="" element={<Home />} />
-          {/* <Route path="/product/:id" element={<Home />} /> */}
           <Route path="cart" element={<Cart />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </DataContext.Provider>
+      </DataContext.Provider> */}
+            <div className="Counter">
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(test())}
+        >
+          +test
+        </button>
+      </div>
+
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './slices/filterSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+    // counterReducer,
+  },
 })
 
 console.log(store)
