f4440bcb7089f1e645212ec6edfbdda202682904
elis-11 <eliza.arzanukaeva@gmail.com>
2022-12-31 20:32:00 +0100

    Lodash-Debounce - Search-Input

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,32 +1,45 @@
-import styles from "./Search.module.scss";
+import debounce from "lodash.debounce";
 import { FiSearch } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
-import { useContext, useRef } from "react";
+import { useCallback, useContext, useRef, useState } from "react";
 import { DataContext } from "../../App";
+import styles from "./Search.module.scss";
 
 export const Search = () => {
-  const { searchValue, setSearchValue } = useContext(DataContext);
+  const [value, setValue] = useState("");
+  const { setSearchValue } = useContext(DataContext);
   const inputRef = useRef();
 
   const clearSearch = () => {
     setSearchValue("");
+    setValue('')
     inputRef.current.focus();
+  };
+
+  // LODASH
+  const updateSearchValue = useCallback(
+    debounce((string) => {
+      setSearchValue(string);
+    }, 250),
+    []
+  );
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+    updateSearchValue(e.target.value);
   };
 
   return (
     <div className={styles.root}>
       <FiSearch className={styles.icon} />
       <input
-        value={searchValue}
         ref={inputRef}
-        onChange={(e) => setSearchValue(e.target.value)}
+        value={value}
+        onChange={onChangeInput}
         type="text"
         placeholder="Search..."
         className={styles.input}
       />
-      {searchValue && (
-        <MdClose className={styles.clear} onClick={clearSearch} />
-      )}
+      {value && <MdClose className={styles.clear} onClick={clearSearch} />}
     </div>
   );
 };
