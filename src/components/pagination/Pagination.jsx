82c37b9cd731dd0cd39020cf1369d4f5483e6ea3
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-03 22:08:35 +0100

    Change Pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -9,7 +9,7 @@ export const Pagination = ({ currentPage, onChangePage }) => {
       nextLabel=">"
       onPageChange={(e) => onChangePage(e.selected + 1)}
       pageRangeDisplayed={4}
-      pageCount={3}
+      pageCount={5}
       forcePage={currentPage - 1}
       previousLabel="<"
       renderOnZeroPageCount={null}
